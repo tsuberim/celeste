8e98f08b0a7ba0d3936d44fdd13cc4ce4146a26b
Matan Tsuberi <tsuberim@gmail.com>
2025-10-22 22:13:32 +0300

    progress

--- train_dit.py
+++ train_dit.py
@@ -395,7 +395,7 @@ def main():
                        help="Batch size for training")
     parser.add_argument("--num_epochs", type=int, default=1000000,
                        help="Number of training epochs")
-    parser.add_argument("--learning_rate", type=float, default=5e-4,
+    parser.add_argument("--learning_rate", type=float, default=2e-4,
                        help="Learning rate")
     parser.add_argument("--save_dir", type=str, default="./models",
                        help="Directory to save model checkpoints")
